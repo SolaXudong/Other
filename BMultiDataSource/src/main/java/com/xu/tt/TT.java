7db3044a3e1a02aaa392214521b593a8a44836d7
SolaXudong <zgc_xudong@163.com>
2020-09-17 15:10:12 +0800

    #

--- BMultiDataSource/src/main/java/com/xu/tt/TT.java
+++ BMultiDataSource/src/main/java/com/xu/tt/TT.java
@@ -1,6 +1,14 @@
 package com.xu.tt;
 
+import java.util.List;
+import java.util.Map;
+
+import javax.sql.DataSource;
+
+import org.apache.commons.collections4.ListUtils;
 import org.junit.Test;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -12,6 +20,17 @@ public class TT {
 
 	public static void main(String[] args) throws Exception {
 		log.info("tt");
+		DataSource ds = DataSourceBuilder.create()
+				//
+				.driverClassName("com.mysql.cj.jdbc.Driver").username("root").password("shijie")
+				//
+				.url("jdbc:mysql://localhost:3306/test?serverTimezone=UTC")
+				//
+				.type(com.zaxxer.hikari.HikariDataSource.class).build();
+		System.out.println(ds.getConnection());
+		JdbcTemplate jt1 = new JdbcTemplate(ds);
+		List<Map<String, Object>> l1 = ListUtils.emptyIfNull(jt1.queryForList("select id, name from user"));
+		log.info("########## l1: count-{}, data-{}", l1.size(), l1);
 	}
 
 	@Test
--- BMultiDataSource/src/test/java/com/xu/tt/ApplicationTests.java
+++ BMultiDataSource/src/test/java/com/xu/tt/ApplicationTests.java
@@ -1,13 +1,17 @@
 package com.xu.tt;
 
-import java.sql.Connection;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
 
 import javax.annotation.Resource;
 import javax.sql.DataSource;
 
+import org.apache.commons.collections4.ListUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import com.alibaba.fastjson.JSONObject;
 import com.xu.tt.service.UserService;
@@ -38,12 +42,25 @@ class ApplicationTests {
 	@Test
 	public void testDataSource() throws Exception {
 		log.info("########## 【test dataSource is ok】");
-		Connection c1 = masterDataSource.getConnection();
-		log.info("########## c1: {}", c1.getMetaData().getURL());
-		Connection c2 = slaveDataSource.getConnection();
-		log.info("########## c2: {}", c2.getMetaData().getURL());
-		Connection c3 = dynamicDataSource.getConnection();
-		log.info("########## c3: {}", c3.getMetaData().getURL());
+		{
+//			Connection c1 = masterDataSource.getConnection();
+//			log.info("########## c1: {}", c1.getMetaData().getURL());
+//			Connection c2 = slaveDataSource.getConnection();
+//			log.info("########## c2: {}", c2.getMetaData().getURL());
+//			Connection c3 = dynamicDataSource.getConnection();
+//			log.info("########## c3: {}", c3.getMetaData().getURL());
+		}
+		{
+			JdbcTemplate jt1 = new JdbcTemplate(masterDataSource);
+			JdbcTemplate jt2 = new JdbcTemplate(slaveDataSource);
+			List<Map<String, Object>> l1 = ListUtils.emptyIfNull(jt1.queryForList("select id, name from user"));
+			log.info("########## l1: count-{}, data-{}", l1.size(), l1);
+			jt1.execute("update user set name = \"" + LocalDateTime.now() + "\" where id = 2");
+			l1 = ListUtils.emptyIfNull(jt1.queryForList("select id, name from user"));
+			log.info("########## l1: count-{}, data-{}", l1.size(), l1);
+			List<Map<String, Object>> l2 = ListUtils.emptyIfNull(jt2.queryForList("select id, name from user"));
+			log.info("########## l2: count-{}, data-{}", l2.size(), l2);
+		}
 	}
 
 	@Test
