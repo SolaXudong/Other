0bcdf2dc7063bb994622ff31102e71c1ad6dc2fa
SolaXudong <zgc_xudong@163.com>
2020-10-16 02:05:26 +0800

    #

--- BMultiDataSource/src/main/java/com/xu/tt/Application.java
+++ BMultiDataSource/src/main/java/com/xu/tt/Application.java
@@ -4,10 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class Application
+//extends SpringBootServletInitializer
+{
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+
+//	@Override
+//	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+//		return builder.sources(Application.class);
+//	}
 
 }
